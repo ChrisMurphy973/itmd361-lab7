207648a07a3d7f1b4d55b95c8e50ed3c0b777155
cmurphy11 <cmurphy11@hawk.iit.edu>
2021-04-12 17:24:51 -0500

    changed html to index, added js, added email/name

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 function init(){
-    //add your javascrip between these two lines of code
-     
-    
-    
-    
-    
-    
-    
-    
-    window.addEventListener('load', init);
+    //add your javascript between these two lines of code
+    var entry = document.getElementById("entrybutton");
+    function myScript() {
+        var textBox = document.getElementById("entryinput");
+        var output = document.getElementById("textoutput");
+        alert("Chris Murphy: " + textBox.value);
+        output.innerHTML = textBox.value;
+    }
+    entry.addEventListener('click', myScript);
+} 
+window.addEventListener('load', init);
